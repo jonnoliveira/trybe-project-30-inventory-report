187d112f88de65d481861aab440733dd65d5829d
jonnoliveira <jonnutri@gmail.com>
2023-07-12 14:27:43 -0300

    Add: test implementation for class product def __str__ - req02

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        "2",
+        "Coca-cola",
+        "Ambev",
+        "01/01/2021",
+        "01/01/2022",
+        "1",
+        "keep it cold",
+    )
+
+    assert str(product.__str__()) == (
+        "The product 2 - Coca-cola "
+        "with serial number 1 "
+        "manufactured on 01/01/2021 "
+        "by the company Ambev "
+        "valid until 01/01/2022 "
+        "must be stored according to the following instructions: "
+        "keep it cold."
+    )

e0f2a2cb3a8d7f150aa468b1373132e17d554303
jonnoliveira <jonnutri@gmail.com>
2023-07-12 14:20:32 -0300

    Add: test implementation for class Product - req01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        "2",
+        "Coca-cola",
+        "Ambev",
+        "01/01/2021",
+        "01/01/2022",
+        "1",
+        "keep it cold",
+    )
+
+    assert product.id == "2"
+    assert product.product_name == "Coca-cola"
+    assert product.company_name == "Ambev"
+    assert product.manufacturing_date == "01/01/2021"
+    assert product.expiration_date == "01/01/2022"
+    assert product.serial_number == "1"
+    assert product.storage_instructions == "keep it cold"

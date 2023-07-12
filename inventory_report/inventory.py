50c0d1569d3e9b017076ee82c986defdfb7b950c
jonnoliveira <jonnutri@gmail.com>
2023-07-12 16:14:48 -0300

    Add: class Inventory implementation - req05

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,19 @@
+from inventory_report.product import Product
+from typing import Optional
+
+
 class Inventory:
-    pass
+    _data: list[Product]
+
+    def __init__(self, data: Optional[list[Product]] = None) -> None:
+        if data is None:
+            self._data = []
+        else:
+            self._data = data
+
+    @property
+    def data(self) -> list[Product]:
+        return self._data
+
+    def add_data(self, data: list[Product]) -> None:
+        self._data.extend(data)

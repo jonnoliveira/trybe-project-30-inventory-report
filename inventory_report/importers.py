c71e635060c1bf07d254d9b6df8f48315b7696db
jonnoliveira <jonnutri@gmail.com>
2023-07-12 15:18:16 -0300

    Add: class implementation for JsonImporter and CsvImporter - req04

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,6 +1,8 @@
 from typing import Dict, Type
 from inventory_report.product import Product
 from abc import ABC, abstractmethod
+import csv
+import json
 
 
 class Importer(ABC):
@@ -10,16 +12,76 @@ class Importer(ABC):
     @abstractmethod
     def import_data(self) -> list[Product]:
         raise NotImplementedError(
-            "Classes derivadas de Importer precisam implementar o método import_data"
+            """Classes derivadas de Importer precisam implementar o método
+            import_data"""
         )
 
 
 class JsonImporter(Importer):
-    pass
+    def __init__(self, path: str) -> None:
+        super().__init__(path)
+
+    def import_data(self) -> list[Product]:
+        list_products = []
+
+        file = open(self.path)
+        data = json.load(file)
+
+        for product in data:
+            list_products.append(
+                Product(
+                    id=product["id"],
+                    product_name=product["product_name"],
+                    company_name=product["company_name"],
+                    manufacturing_date=product["manufacturing_date"],
+                    expiration_date=product["expiration_date"],
+                    serial_number=product["serial_number"],
+                    storage_instructions=product["storage_instructions"],
+                )
+            )
+
+        return list_products
 
 
 class CsvImporter(Importer):
-    pass
+    def __init__(self, path: str) -> None:
+        super().__init__(path)
+
+    def import_data(self) -> list[Product]:
+        list_products = []
+
+        with open(self.path, "r") as data:
+            reader = csv.reader(data)
+            next(reader)
+
+            for line in reader:
+                (
+                    id,
+                    product_name,
+                    company_name,
+                    manufacturing_date,
+                    expiration_date,
+                    serial_number,
+                    storage_instructions,
+                ) = line
+
+                list_products.append(
+                    Product(
+                        id,
+                        product_name,
+                        company_name,
+                        manufacturing_date,
+                        expiration_date,
+                        serial_number,
+                        storage_instructions,
+                    )
+                )
+
+            return list_products
+
+
+a = CsvImporter("inventory_report/data/inventory.csv")
+print(a.import_data())
 
 
 # Não altere a variável abaixo
